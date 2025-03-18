b6fefbcea1c227e08c9fac711e0083f5499d0619
ryanktran23 <ryanktran23@gmail.com>
2025-03-18 01:39:18 -0400

    created API to add food items, list food items, and remove food items

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import foodModel from "../models/foodModel.js";
 import fs from 'fs'
 
@@ -21,7 +22,31 @@ const addFood = async (req, res) => {
         console.log(error)
         res.json({success: false, message:"Error"})
     }
-
 }
 
-export {addFood}
+// all food list
+const listFood = async (req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({success: true, data: foods})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"})
+    }
+}
+
+// remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, ()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"})
+    }
+}
+
+export {addFood, listFood, removeFood}
